3810d8fcd6c8df4ba0401e9092dc3d3fcdfca2b6
kadange <gskadano@gmail.com>
2017-08-08 06:48:12 +0800

    second weigh calculating - not complete

--- JavaTrainingDemo/src/main/java/day6/JavaTraining/Assignments/BallPool.java
+++ JavaTrainingDemo/src/main/java/day6/JavaTraining/Assignments/BallPool.java
@@ -10,26 +10,45 @@ public class BallPool extends Ball{
 //		Ball ball = new Ball();
 		int[] ballPool = createBallPool(BallState.heavy);
 		HashMap<String,int[]> firstWeightResult = new HashMap<String,int[]>();
+		HashMap<String,int[]> secondWeightBallGroup = new HashMap<String,int[]>();
 		
 		for(int i=0;i<ballPool.length;i++) {
 			System.out.println("Ball"+(i+1)+": "+ballPool[i]);
 		}
 		
 		firstWeightResult=firstWeight(ballPool);
-		int[] ballPool2 = new int[8];
-		int counter = 0;
-		for(String key : firstWeightResult.keySet()) {
-			int[] groupVal = firstWeightResult.get(key);
-			for(int i=0;i<groupVal.length;i++) {
-				ballPool2[counter]=groupVal[i];
-				counter++;
-			}
-		}
 		
+		secondWeightBallGroup = readyBallGroup(firstWeightResult);
 		
 		System.out.println("");
-//		secondWeight();
+		calculateSecondWeight(secondWeightBallGroup);
 		
+	}
+	
+	public static HashMap<String,int[]> readyBallGroup(HashMap<String,int[]> ballGroup){
+		HashMap<String,int[]> newBallGroup = new HashMap<String,int[]>();
+		
+		int[] group1 = new int[3];
+		int[] group2 = new int[3];
+		int count = 1;
+		
+		for(String key : ballGroup.keySet()) {
+			int[] groupVal = ballGroup.get(key);
+			if(count==1) {
+				group1[0]=groupVal[0];
+				group2[1]=groupVal[1];
+				group2[2]=groupVal[2];
+				newBallGroup.put(key, group1);
+			}else if(count==2) {
+				group2[0]=groupVal[0];
+				group1[1]=groupVal[1];
+				group1[2]=groupVal[2];
+				newBallGroup.put(key, group2);
+			}
+			count++;
+		}
+		
+		return newBallGroup;
 	}
 	
 	public static void secondWeight() {
@@ -59,6 +78,26 @@ public class BallPool extends Ball{
 		}
 		
 		return groupList;
+	}
+	
+	public static void calculateSecondWeight(HashMap<String,int[]> groupList) {
+		int groupSize = groupList.size(), count=0;
+		int[] groups = new int[groupSize];
+		for(String key : groupList.keySet()) {
+			groups[count]=calculate(groupList.get(key));
+			count++;
+		}
+		
+		weigh(groups[0],groups[1]);
+		String compareResult = Scale.getCompareResult();
+		
+		if(compareResult.equals(Scale.compareEqual)) {
+			groupResult.put("Group2", groupList.get(1));
+			groupResult.put("Group3", groupList.get(2));
+		}else if(compareResult.equals(Scale.compareLeft) || compareResult.equals(Scale.compareRight)) {
+			groupResult.put("Group1", groupList.get(1));
+			groupResult.put("Group2", groupList.get(2));
+		}
 	}
 	
 	public static HashMap<String, int[]> calculateFirstWeight(HashMap<Integer,int[]> groupList) {
