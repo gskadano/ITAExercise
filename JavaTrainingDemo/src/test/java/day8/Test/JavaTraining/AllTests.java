bcd152a3e9260fe15796a0357c5ad3c8e27c9565
kadange <gskadano@gmail.com>
2017-08-09 21:14:26 +0800

    Added Junit

---
+++ JavaTrainingDemo/src/test/java/day8/Test/JavaTraining/AllTests.java
@@ -0,0 +1,11 @@
+package day8.Test.JavaTraining;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ StringHelperTest.class })
+public class AllTests {
+
+}
